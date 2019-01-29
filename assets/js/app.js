9e8fb38e933eb004ae4c7b5a766ae9082fe444c3
Green Man <brendan.mccaughey@gmail.com>
2019-01-28 21:42:36 -0500

    Added scroll magic pins and got rid of white space.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
 //animates boat and reflections using Greensock
 TweenMax.fromTo('#boat', 3, {
   rotation: 5,
@@ -33,7 +35,7 @@ var set = TweenMax.to("#sun", 30, {y: 85, ease: Linear.easeNone});
 var scene = new ScrollMagic.Scene({
 triggerElement:".start",
 offset:100 ,
-duration: 4000})
+duration: 5000})
             .setTween(set)
             .addIndicators()
             .addTo(controller);
@@ -43,7 +45,7 @@ var fade = TweenMax.fromTo('#sun-reflect', 30, {opacity:.347}, {opacity:0});
 var scene = new ScrollMagic.Scene({
 triggerElement:".start",
 offset:100 ,
-duration: 4000})
+duration: 5000})
             .setTween(fade)
             .addIndicators()
             .addTo(controller);
@@ -53,7 +55,34 @@ var color = TweenMax.to('.orange', 30, {fill: "rgb(5, 10, 102)"});
 var scene = new ScrollMagic.Scene({
 triggerElement:".start",
 offset:100 ,
-duration: 4000})
+duration: 5000})
             .setTween(color)
             .addIndicators()
             .addTo(controller);
+
+
+var scene = new ScrollMagic.Scene({
+triggerElement: "#about",
+duration: 500})
+          .setPin("#about")
+          .offset(200)
+          .addIndicators({name: "about"})
+          .addTo(controller);
+
+
+          var scene = new ScrollMagic.Scene({
+            triggerElement: "#work",
+            duration: 500})
+                      .setPin("#work")
+                      .offset(450)
+                      .addIndicators({name: "work"})
+                      .addTo(controller);
+
+                      var scene = new ScrollMagic.Scene({
+                        triggerElement: "#contact",
+                        duration: 500})
+                                  .setPin("#contact")
+                                  .offset(150)
+                                  .addIndicators({name: "contact"})
+                                  .addTo(controller);                     
+});
